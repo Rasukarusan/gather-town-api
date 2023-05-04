2a9e4bb6e89a0efda6dc800cb60d73938fb583ec
Rasukarusan <7gta0you1tube000@gmail.com>
2023-05-04 22:39:49 +0900

    ゴーストモードで障害物を通り抜けるようにした

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import type { Game as Gather, Player } from '@gathertown/gather-game-client'
+import {
+  Game as Gather,
+  Player,
+  SpriteDirectionEnum_ENUM,
+} from '@gathertown/gather-game-client'
 import { App as SlackApp } from '@slack/bolt'
 import * as http from 'http'
 import { initGather } from './gather'
@@ -56,6 +60,33 @@ let slackTs: SlackTs = { date: '', ts: '' }
       // ワープ入り口
       if (player?.x === 31 && player?.y === 42) {
         gather.teleport(MAP_ID, 50, 40, playerId)
+      }
+    })
+
+    // ゴーストのとき障害物を通り抜ける
+    gather.subscribeToEvent('playerGhosts', async (data, context) => {
+      const { player, playerId } = context
+      if (!player || !playerId) return
+      const { direction } = player
+      switch (direction) {
+        case SpriteDirectionEnum_ENUM.Up:
+        case SpriteDirectionEnum_ENUM.UpAlt:
+          gather.teleport(MAP_ID, player.x!, player.y! - 1, playerId, direction)
+          break
+        case SpriteDirectionEnum_ENUM.Down:
+        case SpriteDirectionEnum_ENUM.DownAlt:
+          gather.teleport(MAP_ID, player.x!, player.y! + 1, playerId, direction)
+          break
+        case SpriteDirectionEnum_ENUM.Left:
+        case SpriteDirectionEnum_ENUM.LeftAlt:
+          gather.teleport(MAP_ID, player.x! - 1, player.y!, playerId, direction)
+          break
+        case SpriteDirectionEnum_ENUM.Right:
+        case SpriteDirectionEnum_ENUM.RightAlt:
+          gather.teleport(MAP_ID, player.x! + 1, player.y!, playerId, direction)
+          break
+        default:
+          break
       }
     })
   })
