7ff4793922bb3f6e3be843e0624d260e17563ec6
Rasukarusan <7gta0you1tube000@gmail.com>
2023-05-13 14:47:20 +0900

    playerJoinsの重複対策

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,20 @@ import {
 import { App as SlackApp } from '@slack/bolt'
 import * as http from 'http'
 import { initGather } from './gather'
-import { updateJoinMessage, postJoinMessage } from './message'
+import {
+  updateJoinMessage,
+  postJoinMessage,
+  deleteAllMessages,
+} from './message'
 import { initSlack } from './slack'
 import { SlackTs } from './types'
 const dayjs = require('dayjs')
 
 const MAP_ID = 'office-main'
 const port = process.env.PORT || 8080
+// playerJoinsが1回の入出で複数回実行されることがあり、slackへの投稿が重複してしまうことがある。
+// これを防ぐためのフラグ
+let processing = false
 
 const healthServerListener = () => {
   const server = http.createServer((req, res) => {
@@ -30,14 +37,18 @@ let slackTs: SlackTs = { date: '', ts: '' }
 
   gather.subscribeToConnection((connected) => {
     console.log({ connected })
-    const interval = process.env.APP_ENV === 'development' ? 10000 : 300000
     gather.subscribeToEvent('playerJoins', async (data, context) => {
       console.log('player joined')
+      if (processing) return
+      processing = true
       slackTs = await postJoinMessage(gather, slack)
+      processing = false
     })
 
     gather.subscribeToEvent('playerExits', async (data, context) => {
       console.log('player exit')
+      if (processing) return
+      processing = true
       const today = dayjs().format('YYYY-MM-DD')
       // 本日未投稿の場合
       if (slackTs?.date !== today) {
@@ -45,6 +56,7 @@ let slackTs: SlackTs = { date: '', ts: '' }
       } else {
         slackTs = await updateJoinMessage(gather, slack, slackTs)
       }
+      processing = false
     })
     gather.subscribeToEvent('playerMoves', async (data, context) => {
       const { player, playerId } = context
