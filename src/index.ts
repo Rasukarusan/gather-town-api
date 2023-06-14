73fe3857bbbbf440142ec15d3b99a6bfae787c4e
Rasukarusan <7gta0you1tube000@gmail.com>
2023-06-14 15:42:00 +0900

    謎の移動が発生してしまうので入退出通知以外をコメントアウト

--- src/index.ts
+++ src/index.ts
@@ -63,124 +63,124 @@ let slackTs: SlackTs = { date: '', ts: '' }
       }
       processing = false
     })
-    gather.subscribeToEvent('playerMoves', async (data, context) => {
-      const { player, playerId } = context
-      // gather.setTextStatus(`x: ${player?.x}, y:${player?.y}`, playerId)
-      // テレポート入り口
-      if (player?.x === 31 && player?.y === 34) {
-        gather.teleport(MAP_ID, 31, 40, playerId)
-      }
-      // テレポート出口
-      if (player?.x === 31 && player?.y === 37) {
-        gather.teleport(MAP_ID, 31, 30, playerId)
-      }
-      // ワープ入り口
-      if (player?.x === 31 && player?.y === 42) {
-        gather.teleport(MAP_ID, 50, 40, playerId)
-      }
-    })
+    //   gather.subscribeToEvent('playerMoves', async (data, context) => {
+    //     const { player, playerId } = context
+    //     // gather.setTextStatus(`x: ${player?.x}, y:${player?.y}`, playerId)
+    //     // テレポート入り口
+    //     if (player?.x === 31 && player?.y === 34) {
+    //       gather.teleport(MAP_ID, 31, 40, playerId)
+    //     }
+    //     // テレポート出口
+    //     if (player?.x === 31 && player?.y === 37) {
+    //       gather.teleport(MAP_ID, 31, 30, playerId)
+    //     }
+    //     // ワープ入り口
+    //     if (player?.x === 31 && player?.y === 42) {
+    //       gather.teleport(MAP_ID, 50, 40, playerId)
+    //     }
+    //   })
 
-    // 紙吹雪でプレイヤーを吹き飛ばす
-    gather.subscribeToEvent('playerShootsConfetti', (data, context) => {
-      const shooter = context.player
-      if (!shooter) return
-      const { direction } = shooter
-      switch (direction) {
-        case SpriteDirectionEnum_ENUM.Up:
-        case SpriteDirectionEnum_ENUM.UpAlt: {
-          const players = Object.keys(gather.players).forEach((key) => {
-            const player = gather.players[key]
-            if (player.x === shooter.x && player.y === shooter.y! + -1) {
-              gather.teleport(MAP_ID, player.x!, player.y - 3, key)
-            } else if (
-              player.y === shooter.y! - 2 &&
-              shooter.x! - 1 <= player.x &&
-              player.x <= shooter.x! + 1
-            ) {
-              gather.teleport(MAP_ID, player.x!, player.y - 3, key)
-            }
-          })
-          break
-        }
-        case SpriteDirectionEnum_ENUM.Down:
-        case SpriteDirectionEnum_ENUM.DownAlt: {
-          const players = Object.keys(gather.players).forEach((key) => {
-            const player = gather.players[key]
-            if (player.x === shooter.x && player.y === shooter.y! + 1) {
-              gather.teleport(MAP_ID, player.x!, player.y + 3, key)
-            } else if (
-              player.y === shooter.y! + 2 &&
-              shooter.x! - 1 <= player.x &&
-              player.x <= shooter.x! + 1
-            ) {
-              gather.teleport(MAP_ID, player.x!, player.y + 3, key)
-            }
-          })
-          break
-        }
-        case SpriteDirectionEnum_ENUM.Left:
-        case SpriteDirectionEnum_ENUM.LeftAlt: {
-          const players = Object.keys(gather.players).forEach((key) => {
-            const player = gather.players[key]
-            if (player.x === shooter.x! - 1 && player.y === shooter.y) {
-              gather.teleport(MAP_ID, player.x! - 3, player.y, key)
-            } else if (
-              player.x === shooter.x! - 2 &&
-              shooter.y! - 1 <= player.y &&
-              player.y <= shooter.y! + 1
-            ) {
-              gather.teleport(MAP_ID, player.x! - 3, player.y, key)
-            }
-          })
-          break
-        }
-        case SpriteDirectionEnum_ENUM.Right:
-        case SpriteDirectionEnum_ENUM.RightAlt: {
-          const players = Object.keys(gather.players).forEach((key) => {
-            const player = gather.players[key]
-            if (player.x === shooter.x! + 1 && player.y === shooter.y) {
-              gather.teleport(MAP_ID, player.x! + 3, player.y, key)
-            } else if (
-              player.x === shooter.x! + 2 &&
-              shooter.y! - 1 <= player.y &&
-              player.y <= shooter.y! + 1
-            ) {
-              gather.teleport(MAP_ID, player.x! + 3, player.y, key)
-            }
-          })
-          break
-        }
-        default:
-          break
-      }
-    })
+    //   // 紙吹雪でプレイヤーを吹き飛ばす
+    //   gather.subscribeToEvent('playerShootsConfetti', (data, context) => {
+    //     const shooter = context.player
+    //     if (!shooter) return
+    //     const { direction } = shooter
+    //     switch (direction) {
+    //       case SpriteDirectionEnum_ENUM.Up:
+    //       case SpriteDirectionEnum_ENUM.UpAlt: {
+    //         const players = Object.keys(gather.players).forEach((key) => {
+    //           const player = gather.players[key]
+    //           if (player.x === shooter.x && player.y === shooter.y! + -1) {
+    //             gather.teleport(MAP_ID, player.x!, player.y - 3, key)
+    //           } else if (
+    //             player.y === shooter.y! - 2 &&
+    //             shooter.x! - 1 <= player.x &&
+    //             player.x <= shooter.x! + 1
+    //           ) {
+    //             gather.teleport(MAP_ID, player.x!, player.y - 3, key)
+    //           }
+    //         })
+    //         break
+    //       }
+    //       case SpriteDirectionEnum_ENUM.Down:
+    //       case SpriteDirectionEnum_ENUM.DownAlt: {
+    //         const players = Object.keys(gather.players).forEach((key) => {
+    //           const player = gather.players[key]
+    //           if (player.x === shooter.x && player.y === shooter.y! + 1) {
+    //             gather.teleport(MAP_ID, player.x!, player.y + 3, key)
+    //           } else if (
+    //             player.y === shooter.y! + 2 &&
+    //             shooter.x! - 1 <= player.x &&
+    //             player.x <= shooter.x! + 1
+    //           ) {
+    //             gather.teleport(MAP_ID, player.x!, player.y + 3, key)
+    //           }
+    //         })
+    //         break
+    //       }
+    //       case SpriteDirectionEnum_ENUM.Left:
+    //       case SpriteDirectionEnum_ENUM.LeftAlt: {
+    //         const players = Object.keys(gather.players).forEach((key) => {
+    //           const player = gather.players[key]
+    //           if (player.x === shooter.x! - 1 && player.y === shooter.y) {
+    //             gather.teleport(MAP_ID, player.x! - 3, player.y, key)
+    //           } else if (
+    //             player.x === shooter.x! - 2 &&
+    //             shooter.y! - 1 <= player.y &&
+    //             player.y <= shooter.y! + 1
+    //           ) {
+    //             gather.teleport(MAP_ID, player.x! - 3, player.y, key)
+    //           }
+    //         })
+    //         break
+    //       }
+    //       case SpriteDirectionEnum_ENUM.Right:
+    //       case SpriteDirectionEnum_ENUM.RightAlt: {
+    //         const players = Object.keys(gather.players).forEach((key) => {
+    //           const player = gather.players[key]
+    //           if (player.x === shooter.x! + 1 && player.y === shooter.y) {
+    //             gather.teleport(MAP_ID, player.x! + 3, player.y, key)
+    //           } else if (
+    //             player.x === shooter.x! + 2 &&
+    //             shooter.y! - 1 <= player.y &&
+    //             player.y <= shooter.y! + 1
+    //           ) {
+    //             gather.teleport(MAP_ID, player.x! + 3, player.y, key)
+    //           }
+    //         })
+    //         break
+    //       }
+    //       default:
+    //         break
+    //     }
+    //   })
 
-    // ゴーストのとき障害物を通り抜ける
-    gather.subscribeToEvent('playerGhosts', async (data, context) => {
-      const { player, playerId } = context
-      if (!player || !playerId) return
-      const { direction } = player
-      switch (direction) {
-        case SpriteDirectionEnum_ENUM.Up:
-        case SpriteDirectionEnum_ENUM.UpAlt:
-          gather.teleport(MAP_ID, player.x!, player.y! - 2, playerId, direction)
-          break
-        case SpriteDirectionEnum_ENUM.Down:
-        case SpriteDirectionEnum_ENUM.DownAlt:
-          gather.teleport(MAP_ID, player.x!, player.y! + 2, playerId, direction)
-          break
-        case SpriteDirectionEnum_ENUM.Left:
-        case SpriteDirectionEnum_ENUM.LeftAlt:
-          gather.teleport(MAP_ID, player.x! - 2, player.y!, playerId, direction)
-          break
-        case SpriteDirectionEnum_ENUM.Right:
-        case SpriteDirectionEnum_ENUM.RightAlt:
-          gather.teleport(MAP_ID, player.x! + 2, player.y!, playerId, direction)
-          break
-        default:
-          break
-      }
-    })
+    //   // ゴーストのとき障害物を通り抜ける
+    //   gather.subscribeToEvent('playerGhosts', async (data, context) => {
+    //     const { player, playerId } = context
+    //     if (!player || !playerId) return
+    //     const { direction } = player
+    //     switch (direction) {
+    //       case SpriteDirectionEnum_ENUM.Up:
+    //       case SpriteDirectionEnum_ENUM.UpAlt:
+    //         gather.teleport(MAP_ID, player.x!, player.y! - 2, playerId, direction)
+    //         break
+    //       case SpriteDirectionEnum_ENUM.Down:
+    //       case SpriteDirectionEnum_ENUM.DownAlt:
+    //         gather.teleport(MAP_ID, player.x!, player.y! + 2, playerId, direction)
+    //         break
+    //       case SpriteDirectionEnum_ENUM.Left:
+    //       case SpriteDirectionEnum_ENUM.LeftAlt:
+    //         gather.teleport(MAP_ID, player.x! - 2, player.y!, playerId, direction)
+    //         break
+    //       case SpriteDirectionEnum_ENUM.Right:
+    //       case SpriteDirectionEnum_ENUM.RightAlt:
+    //         gather.teleport(MAP_ID, player.x! + 2, player.y!, playerId, direction)
+    //         break
+    //       default:
+    //         break
+    //     }
+    //   })
   })
   // スラッシュコマンド"/players"を受信
   // @ts-ignore
